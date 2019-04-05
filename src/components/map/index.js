f83550cb4ffb264db7aeb25a6f30c2c65b25ca1a
JoanaRadze <joana.radzeviciute@gmail.com>
2019-04-05 21:55:26 +0300

    trying to get to database Msql...

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -18,7 +18,7 @@ class SimpleMap extends Component {
         return (
             <div style={{ height: '180px', width: '100%' }}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyDXE7jkTghc56LrVXpz2vZlXxGncts95VA" }}
+                    bootstrapURLKeys={{ key: " " }}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                 >
